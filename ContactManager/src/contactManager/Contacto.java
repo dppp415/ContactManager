26e0bf4520da510db62f0bb931ad9540a60ed7fa
david <david@DESKTOP-0DG5CLA>
2024-03-06 20:01:42 +0100

    a

---
+++ ContactManager/src/contactManager/Contacto.java
@@ -0,0 +1,65 @@
+package contactManager;
+
+public class Contacto implements Comparable<Contacto>{
+	
+	private String nombre;
+	private int prefix;
+	private int number;
+	
+	public Contacto() {
+		this.nombre = "";
+		this.prefix = 00;
+		this.number = 000000000;
+	}
+	public Contacto(String nombre, int prefix, int number) {
+		this.nombre = nombre;
+		this.prefix = prefix;
+		this.number = number;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public int getPrefix() {
+		return prefix;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public void setPrefix(int prefix) {
+		this.prefix = prefix;
+	}
+	
+	public String toString() {
+		return "Contacto: " + this.nombre + ", Número: +" + this.prefix + " " + this.number;
+	}
+	
+	@Override
+	public int compareTo(Contacto o) {
+	    int cmp = this.nombre.compareTo(o.nombre);
+	    
+	    if (cmp == 0) {
+	        if (this.number == o.number) {
+	            return 0;
+	        } else if (this.number < o.number) {
+	            return -1;
+	        } else {
+	            return 1;
+	        }
+	    }
+	    return cmp;
+	}
+
+	
+}
---
+++ ContactManager/src/contactManager/Manager.java
@@ -0,0 +1,34 @@
+package contactManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Manager {
+	private ArrayList<Contacto> contactos;
+	
+	public Manager() {
+		this.contactos = new ArrayList<Contacto>();
+	}
+	
+	public boolean add(Contacto a) {
+		return (this.contactos.indexOf(a) == -1) ? this.contactos.add(a): false;
+	}
+	
+	public Contacto remove(Contacto a) {
+		int index = this.contactos.indexOf(a);
+		return (index == -1) ? null: this.contactos.remove(index);
+	}
+	
+	public void clear() {
+		this.contactos.clear();
+	}
+	
+	public void sort() {
+		Collections.sort(this.contactos);
+	}
+	
+	public String toString() {
+		return contactos.toString();
+	}
+	
+}
